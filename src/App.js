a5117a151ffed16a9993b84e7bfcf89be50e298c
vinay1668 <p.vinay1668@gmail.com>
2021-11-12 07:44:46 +0530

    changed app.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         
-          Building the whistle Blower app for my college.
+          Building the whistle Blower app for my college!!!!
          <br />
           (A Twitter like social media Application for my college,but the users can be anonyomous if they want to!)
+
       </header>
     </div>
   );
